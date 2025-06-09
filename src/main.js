b8c67f22caf13b2d92adfae3d64a53969a14642c
dirty-kdk <168411010+dirty-kdk@users.noreply.github.com>
2025-06-10 00:52:33 +0500

    module2-task1

--- src/main.js
+++ src/main.js
@@ -1,59 +1,27 @@
-import PointsModel from './model/points-model.js';
+import {
+  render,
+  RenderPosition
+} from './render.js';
+import TripInfoView from './view/trip.js';
+import FiltersView from './view/filter.js';
+import PointsPresenter from './presenter/board-presenter.js';
+import PointsModel from './model/event-points-model.js';
 import OffersModel from './model/offers-model.js';
 import DestinationsModel from './model/destinations-model.js';
-import RoutePresenter from './presenter/route-presenter.js';
-import FiltersPresenter from './presenter/filters-presenter.js';
-import TripInfoPresenter from './presenter/trip-info-presenter.js';
-import CreatePointPresenter from './presenter/create-point-presenter.js';
-import MainApiService from './service/main-api-service.js';
-import FiltersModel from './model/filters-model.js';
-
-const apiService = new MainApiService();
-
-const pointsModel = new PointsModel(apiService);
-const offersModel = new OffersModel(apiService);
-const destinationsModel = new DestinationsModel(apiService);
-const filtersModel = new FiltersModel();
-
-const pointsContainer = document.querySelector('.trip-events');
-const filtersContainer = document.querySelector('.trip-controls__filters');
 const tripMainContainer = document.querySelector('.trip-main');
-
-const tripInfoPresenter = new TripInfoPresenter({
-  container: tripMainContainer,
-  destinationsModel,
-  offersModel,
-  pointsModel,
-});
-
-const createPointPresenter = new CreatePointPresenter({
-  container: tripMainContainer,
-  editorContainer: pointsContainer,
-  offersModel,
-  destinationsModel,
-});
-
-const routePresenter = new RoutePresenter({
-  container: pointsContainer,
-  createPointPresenter,
+const tripEventsContainer = document.querySelector('.trip-events');
+const filtersContainer = tripMainContainer.querySelector('.trip-controls__filters');
+const pointsModel = new PointsModel();
+const offersModel = new OffersModel();
+const destinationsModel = new DestinationsModel();
+const pointsPresenter = new PointsPresenter({
+  tripEventsContainer,
   pointsModel,
   offersModel,
-  destinationsModel,
-  filtersModel,
+  destinationsModel
 });
 
-const filtersPresenter = new FiltersPresenter({ container: filtersContainer, pointsModel, filtersModel });
+render(new TripInfoView(), tripMainContainer, RenderPosition.AFTERBEGIN);
+render(new FiltersView(), filtersContainer);
 
-const bootstrap = async () => {
-  await Promise.all([
-    offersModel.init(),
-    destinationsModel.init(),
-  ]);
-  pointsModel.init();
-
-  routePresenter.init();
-  filtersPresenter.init();
-  tripInfoPresenter.init();
-};
-
-bootstrap();
+pointsPresenter.init();
--- src/model/offers-model.js
+++ src/model/offers-model.js
@@ -1,48 +1,17 @@
-import { UpdateType } from '../const.js';
-import Observable from '../framework/observable.js';
-import { deleteItem, updateItem } from '../utils.js';
+import {
+  getOffers,
+} from '../mock/offers.js';
 
-export default class OffersModel extends Observable {
-  #service = null;
-  #offers = null;
-
-  constructor(service) {
-    super();
-    this.#service = service;
-  }
-
-  async init() {
-    try {
-      const offers = await this.#service.offers;
-      this.#offers = offers;
-      this._notify(UpdateType.INIT, { data: offers });
-
-    } catch (err) {
-      this.#offers = [];
-      this._notify(UpdateType.INIT, { error: err });
-    }
+export default class OffersModel {
+  constructor() {
+    this.offers = getOffers();
   }
 
   get() {
-    return this.#offers;
+    return this.offers;
   }
 
   getByType(type) {
-    return this.#offers.find((offers) => offers.type === type.toLowerCase()).offers;
-  }
-
-  add(type, offer) {
-    this.#offers.push(offer);
-    this._notify(type, offer);
-  }
-
-  update(type, offer) {
-    this.#offers = updateItem(this.#offers, offer);
-    this._notify(type, offer);
-  }
-
-  delete(type, offer) {
-    this.#offers = deleteItem(this.#offers, offer);
-    this._notify(type, offer);
+    return this.offers.find((offers) => offers.type === type.toLowerCase()).offers;
   }
 }
